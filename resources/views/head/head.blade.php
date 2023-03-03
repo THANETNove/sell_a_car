03ec249e1ac26bb25b82cdf44c90c4073bb954ab
THANETNove <Novel_Manga@outlook.co.th>
2023-03-03 17:27:14 +0700

    อัพ css

---
+++ resources/views/head/head.blade.php
@@ -0,0 +1,22 @@
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>aranoz</title>
+    <link rel="icon" href="{{ URL::asset('img/favicon.png') }}">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+    <!-- animate CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('css/animate.css') }}">
+    <!-- owl carousel CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('css/owl.carousel.min.css') }}">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('css/all.css') }}">
+    <!-- flaticon CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('css/flaticon.css') }}">
+    <link rel="stylesheet" href="{{ URL::asset('css/themify-icons.css') }}">
+    <!-- font awesome CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('css/magnific-popup.css') }}">
+    <!-- swiper CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('css/slick.css') }}">
+    <!-- style CSS -->
+    <link rel="stylesheet" href="{{ URL::asset('css/style.css') }}">
