4a9e8c6fe2eddd7fbda2eb13470546a8541d205b
THANETNove <Novel_Manga@outlook.co.th>
2023-03-28 23:25:58 +0700

    ค้นหา

--- resources/views/head/navbar3.blade.php
+++ resources/views/head/navbar3.blade.php
@@ -4,19 +4,37 @@
     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
         <i class="fa fa-bars"></i>
     </button>
-
-    <!-- Topbar Search -->
-    <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
-        <div class="input-group">
-            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
-                aria-label="Search" aria-describedby="basic-addon2">
-            <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
-                    <i class="fas fa-search fa-sm"></i>
-                </button>
+    <?php
+    $pathname = $_SERVER['REQUEST_URI'];
+    $ex = explode('/', $pathname);
+    
+    $desiredPart = $ex[4];
+    
+    ?>
+    @if (
+        $desiredPart == 'post_products' ||
+            $desiredPart == 'all-products' ||
+            $desiredPart == 'home' ||
+            $desiredPart == 'money-customers')
+        <!-- Topbar Search -->
+        <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search"
+            @if ($desiredPart == 'post_products') action="{{ url('/post_products') }}"
+            @elseif($desiredPart == 'all-products')  action="{{ url('/all-products') }}"
+            @elseif($desiredPart == 'money-customers')  action="{{ url('/money-customers') }}"
+            @elseif($desiredPart == 'home')  action="{{ url('/home') }}" @endif
+            method="post">
+            @csrf
+            <div class="input-group">
+                <input type="text" class="form-control bg-light border-0 small" name="search"
+                    placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit">
+                        <i class="fas fa-search fa-sm"></i>
+                    </button>
+                </div>
             </div>
-        </div>
-    </form>
+        </form>
+    @endif
 
     <!-- Topbar Navbar -->
     <ul class="navbar-nav ml-auto">
