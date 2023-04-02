363cde20170d4ed8f89ba142398d75e855e8b4f0
THANETNove <Novel_Manga@outlook.co.th>
2023-04-02 22:40:42 +0700

    upcode

--- resources/views/head/navbar.blade.php
+++ resources/views/head/navbar.blade.php
@@ -1,4 +1,4 @@
-<header class="">
+{{-- <header class="">
     <div class="">
         <div class="row align-items-center">
             <div class="col-lg-12">
@@ -6,7 +6,12 @@
                     <nav class="navbar fixed-top  bg-primary navbar-expand-lg ">
                         <a class="navbar-brand car-imag2" href="{{ url('/') }}"> <img
                                 src="{{ URL::asset('/img/favicon.png') }}" alt="logo"> </a>
-                        <div class="hearer_icon col-9 col-md-8 col-lg-6">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                            aria-expanded="false" aria-label="Toggle navigation">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="hearer_icon col-9  col-sm-9 col-md-8 col-lg-6">
                             <form class="d-flex" role="search" action="{{ url('searchCar') }}" method="post">
                                 @csrf
                                 <input class="form-control col-12 me-2" type="search" name="search"
@@ -14,7 +19,7 @@
                                 <button class="btn btn-outline-success" type="submit">Search</button>
                             </form>
                         </div>
-                        <div class="" id="navbarSupportedContent">
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav">
                                 <li class="nav-item">
                                     <a class="nav-link navbar-nav-login" href="{{ url('/login') }}">เข้าสู่ระบบ</a>
@@ -30,13 +35,46 @@
             </div>
         </div>
     </div>
-    {{--     <div class="search_input" id="search_input_box">
-        <div class="container ">
-            <form class="d-flex justify-content-between search-inner">
-                <input type="text" class="form-control" id="search_input" placeholder="Search Here">
-                <button type="submit" class="btn">ค้นหา</button>
-                <span class="ti-close" id="close_search" title="Close Search"></span>
+</header>
+ --}}
+
+<nav class="navbar navbar-expand-lg bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                        aria-expanded="false">
+                        Dropdown
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled">Disabled</a>
+                </li>
+            </ul>
+            <form class="d-flex" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
             </form>
         </div>
-    </div> --}}
-</header>
+    </div>
+</nav>
