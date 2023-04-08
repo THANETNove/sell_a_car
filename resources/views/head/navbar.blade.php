5a295e3aeaed8328606845439ef73c96371cac2a
THANETNove <Novel_Manga@outlook.co.th>
2023-04-08 11:29:17 +0700

    asd

--- resources/views/head/navbar.blade.php
+++ resources/views/head/navbar.blade.php
@@ -41,13 +41,14 @@
 </header> --}}
 <nav class="navbar" data-bs-theme="dark">
     <div class="container-fluid">
+
         <a class="navbar-brand car-imag2" href="{{ url('/') }}"> <img src="{{ URL::asset('/img/favicon.png') }}"
                 alt="logo"> </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class=" col-8  col-sm-8 col-md-8 col-lg-8">
+        <div class=" col-8  col-sm-4 col-md-5 col-lg-6">
             <form class="d-flex" role="search" action="{{ url('searchCar') }}" method="post">
                 @csrf
                 <input class="form-control col-12 me-2" type="search" name="search" placeholder="Search"
@@ -55,30 +56,18 @@
                 <button class="btn btn-outline-light" type="submit">Search</button>
             </form>
         </div>
+        <div class="col-sm-4 col-md-3 col-lg-2 right-login">
+            <a href="{{ url('/login') }}" style="color: aliceblue">เข้าสู่ระบบ</a> &nbsp; &nbsp;
+            <a href="{{ url('/register') }}" style="color: aliceblue">สมัครสามชิก</a>
+        </div>
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ url('/login') }}">เข้าสู่ระบบ</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Dropdown
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled">Disabled</a>
+                    <a class="nav-link" href="{{ url('/register') }}">สมัครสามชิก</a>
                 </li>
             </ul>
         </div>
